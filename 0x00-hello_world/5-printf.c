d0280b82606dbadee3db3f0905b99d30314e5b99
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2022-06-30 18:39:38 -0700

    . Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Return: Always 0 (Sucess)
+ * Return: Always ) (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, /n");
+	printf("with proper grammar, but the outcome is a piece of art, \n");
 	return (0);
 }
