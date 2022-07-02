1a9f966859101d68c4dfecc6499e23b055f53ed2
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2022-07-01 18:08:48 -0700

    The last digit 'second edits'

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,12 +12,13 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("last digit of %d is", n);
-	if (n > 5)
+	x = n % 10;
+	printf("last digit of %d is %d ", n, x);
+	if (x > 5)
 		printf("%d and is greater than 5\n", n);
-	else if (n == 0)
+	else if (x == 0)
 		printf("%d and is 0\n", n);
-	else if (n < 6 && n != 0)
+	else if (x < 6 && x != 0)
 		printf("%d and is less than 6 and not 0\n", n);
 	return (0);
 }
