7112c67734c649abcaece662ca676a2da11fc898
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2022-07-07 13:07:24 -0700

    done

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - this is a function that checks for uppercase character
+ * @c: a character argument
+ * Return: returns 1, 0
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a project for More functions, more nested loops
