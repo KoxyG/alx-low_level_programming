efbde982e2aa406f5ca6246ea877dc79d825b4c8
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2022-07-12 10:06:50 -0700

    done

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,9 +2,9 @@
 #include <string.h>
 
 /**
- * _strlen - a function that returns the length of a string
- * @s: a string pointer to be passed to the function
- * Return: length of the string
+ * _strlen -> function to get the length of a string
+ * @s: string pointer to passed to this function
+ * Return: returns length of the string
  */
 int _strlen(char *s)
 {
