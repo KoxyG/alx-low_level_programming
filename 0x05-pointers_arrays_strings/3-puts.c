03c2a9224d0e9ada77a73d68a9841205c355349c
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2022-07-12 11:30:48 -0700

    done

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - a function that prints a string
+ * @str: a string
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++
+	}
+	_putchar('\n');
+}
