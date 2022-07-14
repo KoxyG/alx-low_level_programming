8d8c8b5a4efaa60f676c2c07432025a64d4821b9
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2022-07-14 15:00:04 -0700

    done

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * reverse_array - reversing an array
+ * @a: an array
+ * @n: elememt of an array
+ */
+void reverse_array(int *a, int n)
+{
+	for (int i = 0, j = n - 1; i < j; i++, j--)
+	{
+		int temp = arr[i];
+
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+}
