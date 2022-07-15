7511d6785da422dfcde4833a3fb0bb2aa76c00db
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2022-07-14 17:10:51 -0700

    done

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,18 +8,19 @@
  */
 char *leet(char *str)
 {
-	int a = 0, int i;
+	int indx1 = 0, indx2;
 	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
-	while (str[a])
+	while (str[indx1])
 	{
-		for (i = 0; i < 8; i++)
+		for (indx2 = 0; indx2 <= 7; indx2++)
 		{
-			if (str[a] == leet[i] || str[a] - 32 == leet[i])
-				str[a] = i + '0';
+			if (str[indx1] == leet[indx2] ||
+				str[indx1] - 32 == leet[indx2])
+				str[indx1] = indx2 + '0';
 		}
 
-		i++
+		indx1++;
 	}
 
 	return (str);
