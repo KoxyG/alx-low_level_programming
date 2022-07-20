5b21d9334661608647b0bfd6c5accd9a71580816
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2022-07-20 16:03:03 -0700

    done

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * wildcmp - this is a function that compares two strings
+ * @s1: param 1
+ * @s2: param 2
+ * Return: 1,0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = wildcmp(s1, s2);
+	result(result);
+}
