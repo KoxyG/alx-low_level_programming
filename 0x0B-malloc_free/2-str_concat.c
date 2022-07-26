434522e3c48ef87385ac3b6af8420a16e473bf5c
KoxyG <106968712+KoxyG@users.noreply.github.com>
2022-07-26 21:00:33 +0100

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,49 +1,41 @@
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * *str_concat - concatenates two strings
- * @s1: string to concatenate
- * @s2: other string to concatenate
- *
- * Return: pointer to the new string created (Success), or NULL (Error)
- */
+* str_concat -> string concatinating function
+* @s1: string 1
+* @s2: string 2
+* Return: string 1 + string 2
+*/
 char *str_concat(char *s1, char *s2)
 {
-	char *s3;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	int i = 0, j = 0, l = 0, k = 0;
+	char *s;
 
-	while (s1 && s1[len1])
-		len1++;
-	while (s2 && s2[len2])
-		len2++;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[i])
+		i++;
+	while (s2[j])
+		j++;
 
-	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (s3 == NULL)
+	l = i + j;
+	s = (char *)malloc(l * sizeof(char) + 1);
+	if (s == NULL)
 		return (NULL);
-
-	i = 0;
 	j = 0;
-
-	if (s1)
+	while (k < l)
 	{
-		while (i < len1)
+		if (k < i)
+			s[k] = s1[k];
+		if (k >= i)
 		{
-			s3[i] = s1[i];
-			i++;
-		}
-	}
-
-	if (s2)
-	{
-		while (i < (len1 + len2))
-		{
-			s3[i] = s2[j];
-			i++;
+			s[k] = s2[j];
 			j++;
 		}
+		k++;
 	}
-	s3[i] = '\0';
-
-	return (s3);
+	s[k] = '\0';
+	return (s);
 }
