056e465c1da95a3245b33d8198fabb48d3377322
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2022-08-01 13:20:45 -0700

    done

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of a file it was compiled
+ * folloewd by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
