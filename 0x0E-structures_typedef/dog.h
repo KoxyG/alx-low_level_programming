d6bd436de0129fd515827aeafe949bd6321f5065
KoxyG <106968712+KoxyG@users.noreply.github.com>
2022-08-02 00:06:21 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
- * struct dog - A new type discribing a dog.
- * @name: The name of the dog
- * @age: The age of the dog
- * @owner: The owner of the dog
- *
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
  */
 struct dog
 {
 	char *name;
-	float *age;
+	float age;
 	char *owner;
 };
 
