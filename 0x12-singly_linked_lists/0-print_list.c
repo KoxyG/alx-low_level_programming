747e93aec303e37dec0d0690e420639f11d93a4b
Progress ochuko Eyaadah <koxygraphicsgmail.com>
2022-08-11 13:41:00 -0700

    done

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,17 +12,17 @@ size_t print_list(const list_t *h)
 {
 	size_t node = 0;
 
-	while (h->next != NULL)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
-			printf("%d %s\n", 0, "nil");
+			printf("[%d] %s\n", 0, "(nil)");
 		}
 		else
 
-			printf("%d", h->len);
-				h = h->next;
-			node++;
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		node++;
 	}
 	return (node);
 }
