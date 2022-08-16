e8d02b8af68d78933c508c597d5d89ccda6ab2f1
Progress ochuko Eyaadah <koxygraphicsgmail.com>
2022-08-15 17:50:41 -0700

    done

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - a function that frees a linked list
+ * @head: the linked list
+ *
+ * Return: 0
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *new;
+
+	while (head != NULL)
+	{
+		new = head;
+		head = head->next;
+		free(new);
+	}
+
+}
