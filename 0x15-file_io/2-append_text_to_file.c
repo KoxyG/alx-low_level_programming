cba8d0e869f8d8383389d600fdf471de761f357b
Progress ochuko Eyaadah <koxygraphicsgmail.com>
2022-08-22 18:36:00 -0700

    done

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - a function that appends text at the end of a file.
+ * @filename: the name of the file
+ * @text_content: text to write in the file
+ *
+ * Return: return 1 on success and -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+
+	int fd;
+	int w, i;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+	if (text_content)
+	{
+		while (text_content[i])
+			i++;
+
+		w = write(fd, text_content, i);
+		if (w != i)
+			return (0);
+	}
+	close(fd);
+	return (1);
+}
