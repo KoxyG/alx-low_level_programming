a6ff565162f6fb5827d042d60596e6a0dcf66a09
Progress ochuko Eyaadah <koxygraphicsgmail.com>
2022-08-22 18:47:51 -0700

    done

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -27,7 +27,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 		w = write(fd, text_content, i);
 		if (w != i)
-			return (0);
+			return (-1);
 	}
 	close(fd);
 	return (1);
