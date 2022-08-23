ca1c3b2f91609d34842a2e6e017e385d8dac9c4f
Progress ochuko Eyaadah <koxygraphicsgmail.com>
2022-08-22 17:51:32 -0700

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * create_file - a function that creates a file
- * @filename - the name of file to create
+ * create_file - creates a file and fill it with text
+ * @filename - name of file to create
  * @text_content - a string to write to the file
  *
  * Return: returns 1 on success, -1 on failure
@@ -12,11 +12,10 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	int t, i;
 
-	fd = open("text_content", O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
-	
 	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
@@ -28,8 +27,6 @@ int create_file(const char *filename, char *text_content)
 				return (-1);
 
 	}
-	close (fd);
-	
-	return (1);	
-
+	close(fd);
+	return (1);
 }
